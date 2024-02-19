fc748cb31310ab0cfbbdbfc22901392f87bdc2f1
Guilherme B <dogui0202@gmail.com>
2024-02-19 15:01:01 -0300

    Site finalizado, agora trocar as branchs e enviar para avaliação

---
+++ index.js
@@ -0,0 +1,30 @@
+function playBateria() {
+    let bateria = document.getElementById("bateria");
+    bateria.play();
+}
+
+function playFlauta() {
+    let flauta = document.getElementById("flauta");
+    flauta.play();
+}
+
+function playGaita() {
+    let gaita = document.getElementById("gaita");
+    gaita.play();
+}
+
+function playPiano() {
+    let piano = document.getElementById("piano");
+    piano.play();
+}
+
+function playViola() {
+    let viola = document.getElementById("viola");
+    viola.play();
+}
+
+function playViolino() {
+    let violino = document.getElementById("violino");
+    violino.play();
+}
+
